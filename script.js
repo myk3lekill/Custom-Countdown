e5703d249ee7e92096aafd969a80ccef6c43e58e
me <me@gmail.com>
2023-09-18 16:54:14 +0200

    fifth commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('javascript is connected')
+//DOM
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+
+//Set Date Input Min with Today's Date
+const today = new Date().toISOString().split('T')[0];
+console.log('today\'s date', today)
+dateEl.setAttribute('min', today)
