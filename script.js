e3d9589c3ab853af6ee97c53cf5234fc954bc715
me <me@gmail.com>
2023-09-18 17:14:38 +0200

    sixth commit

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 
 //Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 console.log('today\'s date', today)
 dateEl.setAttribute('min', today)
+
+//Take values from from input
+function updateCountdown(e) {
+    e.preventDefault(); //preventDefault method prevent the refresh of the page at submit's press
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value
+    console.log(countdownTitle, countdownDate)
+}
+
+//Event Listeners
+countdownForm.addEventListener('submit', updateCountdown)
